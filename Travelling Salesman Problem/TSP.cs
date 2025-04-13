b58bbe8f43a5ac6a375581364b8bc95b91a8d09d
Apshai Ahata <aapshai@kse.org.ua>
2025-04-13 22:50:32 +0300

    фінал

--- Travelling Salesman Problem/TSP.cs
+++ Travelling Salesman Problem/TSP.cs
@@ -2,12 +2,12 @@
 
 public class TSP
 {
-    public static (List<int>, int) GreedyTSP(Graph graph)
+    public static (List<int>, int) TSPGreedy(Graph graph)
     {
         int n = graph.vertices;
         bool[] visited = new bool[n];
         List<int> path = new List<int>();
-        int totalCost = 0;
+        int Cost = 0;
 
         int current = 0;
         path.Add(current);
@@ -20,28 +20,28 @@ public class TSP
 
             for (int v = 0; v < n; v++)
             {
-                if (!visited[v] && graph.adjacencyMatrix[current, v] != 0 && graph.adjacencyMatrix[current, v] < minCost)
+                if (!visited[v] && graph.adjMatrix[current, v] != 0 && graph.adjMatrix[current, v] < minCost)
                 {
-                    minCost = graph.adjacencyMatrix[current, v];
+                    minCost = graph.adjMatrix[current, v];
                     next = v;
                 }
             }
 
             if (next == -1)
-                throw new Exception("Граф не є повним, неможливо знайти шлях");
+                throw new Exception("Граф не повний, тому неможливо знайти шлях");
 
             visited[next] = true;
             path.Add(next);
-            totalCost += minCost;
+            Cost += minCost;
             current = next;
         }
 
-        totalCost += graph.adjacencyMatrix[current, path[0]]; 
+        Cost += graph.adjMatrix[current, path[0]]; 
         path.Add(path[0]);
 
-        return (path, totalCost);
+        return (path, Cost);
     }
-    public static (List<int>, int) GreedyTSP_List(Graph graph)
+    public static (List<int>, int) TSPList(Graph graph)
     {
         int n = graph.vertices;
         bool[] visited = new bool[n];
@@ -57,7 +57,7 @@ public class TSP
             int next = -1;
             int minCost = int.MaxValue;
 
-            foreach (var (v, weight) in graph.adjacencyList[current])
+            foreach (var (v, weight) in graph.adjList[current])
             {
                 if (!visited[v] && weight < minCost)
                 {
@@ -67,7 +67,7 @@ public class TSP
             }
 
             if (next == -1)
-                throw new Exception("Граф не є повним, неможливо знайти шлях");
+                throw new Exception("Граф не повний, тому неможливо знайти шлях");
 
             visited[next] = true;
             path.Add(next);
@@ -75,7 +75,7 @@ public class TSP
             current = next;
         }
 
-        foreach (var (v, weight) in graph.adjacencyList[current])
+        foreach (var (v, weight) in graph.adjList[current])
         {
             if (v == path[0])
             {
@@ -87,7 +87,4 @@ public class TSP
 
         return (path, totalCost);
     }
-
-
-    
 }
